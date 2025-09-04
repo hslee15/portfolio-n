005aa2f9625313c775226c350591674520499196
hslee15 <lhs01019@kbu.ac.kr>
2025-09-04 20:12:38 +0900

    0904

--- frontend/src/components/home/Footer.jsx
+++ frontend/src/components/home/Footer.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import footer from '../../utils/footer'
+import {FaGithub} from "react-icons/fa"
+import {SiNotion} from "react-icons/si"
+import "./styles/Footer.scss"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='inner foot-inner'>
+      <div className="left">
+        <div className='logo'>LOGO</div>
+        <p>@ 2025 DECODE.Lab. All rights reserved.</p>
+      </div>
+      <div className="right">
+        <a href="">
+          <FaGithub size={28}/>
+        </a>
+        <a href="">
+          <SiNotion size={28}/>
+        </a>
+      </div>
+    </div>
   )
 }
 
